b703a479ae15dd51d94f0a2dadff79b803e0c925
Chinmoy Anand Patra <chinmoyanand.patra@gmail.com>
2020-09-04 18:05:07 +0530

    adding all test cases

--- src/test/java/service/PromotionServiceTest.java
+++ src/test/java/service/PromotionServiceTest.java
@@ -39,6 +39,31 @@ public class PromotionServiceTest {
 	}
 	
 	@Test
+	public void calculateFinalPriceAfterPromotionTest2() {
+		Product productA = new Product(SKU.A,"Product A is added",BigDecimal.valueOf(50));
+		Product productB = new Product(SKU.B,"Product B is added",BigDecimal.valueOf(30));
+		Product productC = new Product(SKU.C,"Product C is added",BigDecimal.valueOf(20));
+		List<Product> products = Arrays.asList(productA,productA,productA,productA,productA,productB,productB,productB,productB,productB,productC);
+		Cart cart = new Cart(products);
+		List<Promotion> promotions = Arrays.asList(new Promotion(true,PromotionName.THREEA,BigDecimal.valueOf(130)),
+				new Promotion(true,PromotionName.TWOB,BigDecimal.valueOf(45)),new Promotion(true,PromotionName.CD,BigDecimal.valueOf(30)));
+		assertEquals(BigDecimal.valueOf(370), service.calculateFinalCartPrice(cart, promotions));
+	}
+	
+	@Test
+	public void calculateFinalPriceAfterPromotionTest3() {
+		Product productA = new Product(SKU.A,"Product A is added",BigDecimal.valueOf(50));
+		Product productB = new Product(SKU.B,"Product B is added",BigDecimal.valueOf(30));
+		Product productD = new Product(SKU.D,"Product D is added",BigDecimal.valueOf(15));
+		Product productC = new Product(SKU.C,"Product C is added",BigDecimal.valueOf(20));
+		List<Product> products = Arrays.asList(productA,productA,productA,productB,productB,productB,productB,productB,productC,productD);
+		Cart cart = new Cart(products);
+		List<Promotion> promotions = Arrays.asList(new Promotion(true,PromotionName.THREEA,BigDecimal.valueOf(130)),
+				new Promotion(true,PromotionName.TWOB,BigDecimal.valueOf(45)),new Promotion(true,PromotionName.CD,BigDecimal.valueOf(30)));
+		assertEquals(BigDecimal.valueOf(280), service.calculateFinalCartPrice(cart, promotions));
+	}
+	
+	@Test
 	public void calculateFinalPriceWithoutPromotionTest() {
 		List<Product> products = Arrays.asList(productA,productB,productC);
 		Cart cart = new Cart(products);
